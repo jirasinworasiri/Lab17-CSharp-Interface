e6447f8ed35324f6ff7962f06d8d41eaf21b755d
jirasinworasiri <aomjirasin457@gmail.com>
2025-03-27 21:33:46 +0700

    66030029 จิรสิน

---
+++ Lab17_Ex04/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+interface ISpeakable
+{
+    void Speak();
+}
+
+abstract class Animal : IMovable, ISpeakable
+{
+    public abstract void Move();
+    public abstract void Speak(); // ต้องเพิ่มเมทอด Speak() ให้ Animal
+}
+
+class Dog : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Dog moves by running on the ground");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Fish : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Fish moves by swimming in the water");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Fish does not make a sound");
+    }
+}
+
+class Bird : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Bird moves by flying in the air");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Bird chirps");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal[] animals = new Animal[3];
+        animals[0] = new Dog();
+        animals[1] = new Bird();
+        animals[2] = new Fish();
+
+        foreach (var animal in animals)
+        {
+            animal.Move();
+            animal.Speak(); 
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab17_Ex06/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+interface IRemoteControl
+{
+    void TurnOn();
+    void TurnOff();
+    void ChannelUp();
+    void ChannelDown();
+}
+
+abstract class PowerAppliance
+{
+    public bool PowerStatus { get; set; } = false; // ตั้งค่าเริ่มต้นเป็นปิด
+    public int Wattage { get; set; }
+}
+
+abstract class Television : PowerAppliance, IRemoteControl
+{
+    public int Channel { get; set; } = 1;
+
+    public virtual void TurnOn() { Console.WriteLine("TV Turn on"); PowerStatus = true; }
+    public virtual void TurnOff() { Console.WriteLine("TV Turn off"); PowerStatus = false; }
+    public virtual void ChannelUp() { Console.WriteLine($"TV Channel up: {++Channel}"); }
+    public virtual void ChannelDown() { Console.WriteLine($"TV Channel down: {--Channel}"); }
+}
+
+class SonyTV : Television
+{
+    public override void TurnOn() { Console.WriteLine("Sony TV Turn on"); PowerStatus = true; }
+    public override void TurnOff() { Console.WriteLine("Sony TV Turn off"); PowerStatus = false; }
+    public override void ChannelUp() { Console.WriteLine($"Sony TV Channel up: {++Channel}"); }
+    public override void ChannelDown() { Console.WriteLine($"Sony TV Channel down: {--Channel}"); }
+}
+
+class Lamp : PowerAppliance
+{
+    public void TurnOn() { Console.WriteLine("Lamp Turn on"); PowerStatus = true; }
+    public void TurnOff() { Console.WriteLine("Lamp Turn off"); PowerStatus = false; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Television myTV = new SonyTV();
+        myTV.Wattage = 100;
+        myTV.TurnOn();
+        myTV.ChannelUp();
+        myTV.ChannelDown();
+        myTV.TurnOff();
+
+        Lamp myLamp = new Lamp();
+        myLamp.Wattage = 50;
+        myLamp.TurnOn();
+        myLamp.TurnOff();
+    }
+}
